c22584040d1ff0f4552e929b5eee70f5536cb5d2
Christopher Bailey <seabayley@outlook.com>
2020-06-05 10:35:28 -0500

    Added ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,11 @@
 def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
-
-    return None
+    c = {}
+    for i, v in enumerate(weights):
+        if v in c and v*2 == limit:
+            return i, c[v]
+        else:
+            c[v] = i
+    print(c)
+    for i in c:
+        if limit - i in c:
+            return sorted([c[i], c[limit-i]], reverse=True)
