e89b1e13f5a2cb61bd4ac9fe2e53def894043b68
Christopher Bailey <seabayley@outlook.com>
2020-06-05 12:38:21 -0500

    Added ex5

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,14 +1,18 @@
 # Your code here
 
 
-
 def finder(files, queries):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    # return [x for x in files for y in queries if y in x] # For short lists.
+    c = {}
+    results = []
+    for f in files:
+        c.setdefault(f.rsplit('/')[-1], []).append(f)
 
-    return result
+    for q in queries:
+        if q in c.keys():
+            for fn in c[q]:
+                results.append(fn)
+    return results
 
 
 if __name__ == "__main__":
